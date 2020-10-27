3b92473cec825d4014c5201a03acb26d8b4b31ab
pemeraldy <pemeraldy@gmail.com>
2020-10-27 11:46:46 +0100

    for deploy

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const Api = axios.create({
-    baseURL: 'http://localhost:4000'
-    // baseURL: 'https://yopee-vue-store.herokuapp.com',
+    // baseURL: 'http://localhost:4000'
+    baseURL: 'https://yopee-vue-store.herokuapp.com',
 })
 
 export default Api
